31a91e8adc58ba93d733c18b88fe01bf813b9e14
ialeman <iaforapps182@gmail.com>
2023-11-28 23:48:02 -0700

    Added Marvel Api Test

---
+++ src/test/java/com/sngular/marvel/MarvelApiTest.java
@@ -0,0 +1,36 @@
+package com.sngular.marvel;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.test.context.SpringBootTest;
+import com.sngular.marvel.controller.MarvelApi;
+import com.sngular.marvel.models.MarvelPage;
+
+@SpringBootTest
+public class MarvelApiTest {
+
+	@Value("${marvel.api.private-key}")
+    private String apiPrivateKey;
+    
+    @Value("${marvel.api.public-key}")
+    private String apiPublicKey;
+    
+	@Test
+    public void testGetCharacters() {
+        MarvelApi marvelApiClient = new MarvelApi(apiPrivateKey, apiPublicKey);
+        MarvelPage page = marvelApiClient.getCharacters();
+        System.out.println(page.toString());
+        assertNotNull(page);
+    }
+
+    @Test
+    public void testGetCharacterById() {
+    	MarvelApi marvelApiClient = new MarvelApi(apiPrivateKey, apiPublicKey);
+        MarvelPage page = marvelApiClient.getCharacterById(1012717);
+        System.out.println(page.toString());
+        assertNotNull(page);
+    }
+	
+}
